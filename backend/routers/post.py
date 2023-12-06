f5a08606fe298b1cbda06b3bd429e9b347581580
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-12-06 15:48:54 +0900

    feat : 글 삭제, 수정

--- backend/routers/post.py
+++ backend/routers/post.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
+from starlette import status
 
 from database import (
     create_server_connection,
@@ -34,3 +35,50 @@ def create_post(post_request: PostRequest, user: user_dependency):
         return {"message": "Post created successfully"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Failed to create post: {str(e)}")
+
+
+@router.put("/update/{post_id}")
+def update_post(post_request: PostRequest, user: user_dependency, post_id: int):
+    user_id = user.get("id")
+    post = get_post(post_id)
+    if post["author_id"] != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User not authorized to delete this post",
+        )
+    connection = create_server_connection()
+    cursor = connection.cursor(dictionary=True)
+    # Update post title / content
+    query = """
+        UPDATE post
+        SET title = %s, content = %s
+        WHERE post_id = %s;
+        """
+    cursor.execute(query, (post_request.title, post_request.content, post_id))
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return {"detail": "Post updated successfully"}
+
+
+@router.delete("/delete")
+def delete_post(post_id: int, user: user_dependency):
+    user_id = user.get("id")
+    post = get_post(post_id)
+    if post["author_id"] != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User not authorized to delete this post",
+        )
+    connection = create_server_connection()
+
+    # Delete the post
+    cursor = connection.cursor()
+    query = "DELETE FROM post WHERE post_id = %s;"
+    cursor.execute(query, (post_id,))
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return {"detail": "Post deleted successfully"}
